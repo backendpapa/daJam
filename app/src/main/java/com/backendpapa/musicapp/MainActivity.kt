275fe2a3eab9b01d3715ca0e9342e946c7b98fbb
Akeju Paul <akejupaul10@gmail.com>
2021-07-19 02:54:22 +0100

    make it better

--- app/src/main/java/com/backendpapa/musicapp/MainActivity.kt
+++ app/src/main/java/com/backendpapa/musicapp/MainActivity.kt
@@ -3,13 +3,25 @@ package com.backendpapa.musicapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         var mediaPlayer = MediaPlayer.create(this, R.raw.koffee)
-        mediaPlayer.start()
+        findViewById<ImageButton>(R.id.play).setOnClickListener{
+
+              mediaPlayer=MediaPlayer.create(this, R.raw.koffee)
+
+            mediaPlayer.start()
+        }
+        findViewById<ImageButton>(R.id.stop).setOnClickListener{
+            mediaPlayer.stop()
+        }
+//
     }
 }
